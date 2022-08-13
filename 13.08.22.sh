0f3bdeb3c6323d57b62e1ab60d486106ada3ddc7
Shane Mulligan <mullikine@gmail.com>
2022-08-13 21:48:41 +1200

    13.08.22

---
+++ 13.08.22.sh
@@ -0,0 +1,40 @@
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  npx --no-install postcss --config /home/shane/var/smulliga/source/git/metasophia/metasophia-hugo/themes/pico/assets/config/postcss.config.js # "<==" hugo
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm use "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm install "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  npx --no-install postcss --config /home/shane/var/smulliga/source/git/metasophia/metasophia-hugo/themes/pico/assets/config/postcss.config.js # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm use "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm install "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  npx --no-install postcss --config /home/shane/var/smulliga/source/git/metasophia/metasophia-hugo/themes/pico/assets/config/postcss.config.js # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm use "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm install "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  npx --no-install postcss --config /home/shane/var/smulliga/source/git/metasophia/metasophia-hugo/themes/pico/assets/config/postcss.config.js # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm use "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm install "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  npx --no-install postcss --config /home/shane/var/smulliga/source/git/metasophia/metasophia-hugo/themes/pico/assets/config/postcss.config.js # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm use "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm install "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  npx --no-install postcss --config /home/shane/var/smulliga/source/git/metasophia/metasophia-hugo/themes/pico/assets/config/postcss.config.js # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm use "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm install "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  npx --no-install postcss --config /home/shane/var/smulliga/source/git/metasophia/metasophia-hugo/themes/pico/assets/config/postcss.config.js # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm use "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm install "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  npx --no-install postcss --config /home/shane/var/smulliga/source/git/metasophia/metasophia-hugo/themes/pico/assets/config/postcss.config.js # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm use "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm install "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  npx --no-install postcss --config /home/shane/var/smulliga/source/git/metasophia/metasophia-hugo/themes/pico/assets/config/postcss.config.js # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm use "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm install "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  npx --no-install postcss --config /home/shane/var/smulliga/source/git/metasophia/metasophia-hugo/themes/pico/assets/config/postcss.config.js # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm use "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm install "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  npx --no-install postcss --config /home/shane/var/smulliga/source/git/metasophia/metasophia-hugo/themes/pico/assets/config/postcss.config.js # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm use "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm install "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  npx --no-install postcss --config /home/shane/var/smulliga/source/git/metasophia/metasophia-hugo/themes/pico/assets/config/postcss.config.js # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm use "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm install "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  npx --no-install postcss --config /home/shane/var/smulliga/source/git/metasophia/metasophia-hugo/themes/pico/assets/config/postcss.config.js # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm use "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  nvm install "" # "<==" npx
+cd "/home/shane/var/smulliga/source/git/metasophia/metasophia-hugo";  npx --no-install postcss --config /home/shane/var/smulliga/source/git/metasophia/metasophia-hugo/themes/pico/assets/config/postcss.config.js # "<==" npx
